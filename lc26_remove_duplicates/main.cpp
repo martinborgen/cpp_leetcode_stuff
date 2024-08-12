c0519c3756a6a067889035ff55c5573b8eefb2f7
Martin Borgén <martinb.nymail@gmail.com>
2024-08-12 20:31:46 +0200

    Versions passes tests, though seems to allocate outside the nums array sometimes

--- lc26_remove_duplicates/main.cpp
+++ lc26_remove_duplicates/main.cpp
@@ -4,16 +4,48 @@
 class Solution {
 public:
     int removeDuplicates(std::vector<int>& nums) {
-        int len = nums.size();
-        std::cout << len;
-        return len;
+        if (nums.size() == 1) {
+            return 1;
+        }
+        int cnt = 1;
+        int left = 0;
+        int right = cnt;
+        while (cnt <= nums.size()) {
+            while (right < nums.size() && nums[left] == nums[right]) {
+                right++;
+            }
+            
+            if (nums[left] != nums[right]) {
+                nums[cnt] = nums[right];
+                left = cnt;
+                cnt++;
+            } else {
+                break;
+            }
+        }
+        return cnt - 1;
     }
 };
 
 int main() {
     Solution s;
-    std::vector<int> vec = {1,1,2};
+    std::vector<int> vec1 = {1,1,2};
 
-    int res = s.removeDuplicates(vec);
+    int res1 = s.removeDuplicates(vec1);
+    printf("%d\n", res1);
+
+    std::vector<int> vec2 = {0,0,1,1,1,2,2,3,3,4};
+    int res2 = s.removeDuplicates(vec2);
+    printf("%d\n", res2);
+
+    std::vector<int> vec3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int res3 = s.removeDuplicates(vec3);
+    printf("%d\n", res3);
+
+    std::vector<int> vec4 = {0, 0, 0, 0, 0, 0, 0};
+    int res4 = s.removeDuplicates(vec4);
+    printf("%d\n", res4);
+
+    std::cout << "\n";
     return 0;
 }
