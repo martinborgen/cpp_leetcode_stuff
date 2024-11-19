b76794edfcfa085e089dd0231404576d2a1a23bd
Martin Borgén <martinb.nymail@gmail.com>
2024-11-19 17:16:12 +0100

    added two more edge casese.

--- lc54_spiral_matrix/main.cpp
+++ lc54_spiral_matrix/main.cpp
@@ -85,4 +85,12 @@ int main() {
     std::vector<std::vector<int>> test1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {8, 9, 11, 12}};
     std::vector<int> res1 = s.spiralOrder(test1);
     print_vect(res1);
+
+    std::vector<std::vector<int>> test2 = {{1, 2, 3, 4}};
+    std::vector<int> res2 = s.spiralOrder(test2);
+    print_vect(res2);
+
+    std::vector<std::vector<int>> test3 = {{4}, {8}, {12}};
+    std::vector<int> res3 = s.spiralOrder(test3);
+    print_vect(res3);
 }
